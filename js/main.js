eec9a956020ca034e0e4dda1d22ae9fc3974c837
Arthur Schopenhauer <lokigoldx8@gmail.com>
2022-04-13 20:08:01 +0300

    Закончил сайт

--- js/main.js
+++ js/main.js
@@ -4,4 +4,14 @@ $(function () {
 			// duration: 2000
 	}
 	});
+
+	$('.slider-blog__inner').slick({
+		dots: true,
+		prevArrow: `<button type="button" class="slick-prev">
+			<img src="images/arrow-left.svg" alt="">
+		</button>`,
+		nextArrow: `<button type="button" class="slick-next">
+			<img src="images/arrow-right.svg" alt="">
+		</button>`
+	});
 });
