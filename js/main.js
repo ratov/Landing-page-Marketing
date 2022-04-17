836e02bcb3528d46afb2ba56a0732d5b1b4db50d
Arthur Schopenhauer <lokigoldx8@gmail.com>
2022-04-17 15:40:44 +0300

    Реализовал плавный скролл по сайту, путем создания якоря и приминении jquery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(function () {
 
+	//ПЛАВНЫЙ СКРОЛЛ
+	$(".menu a").on("click", function (e) {
+		e.preventDefault();
+		//забираем идентификатор бока с атрибута href
+		var id = $(this).attr('href'),
+			//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+			// console.log(id)
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({ scrollTop: top }, 1500);
+	});
+
 	$('.slider-blog__inner').slick({
 		dots: true,
 		prevArrow: `<button type="button" class="slick-prev">
